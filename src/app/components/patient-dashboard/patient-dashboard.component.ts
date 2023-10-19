074f5cbbda8000bff505efcddf8571f726ec3fee
kailash sunny <121025635+ksks122@users.noreply.github.com>
2023-10-18 20:18:00 -0500

    Modified_patient-dashboard.component.ts

--- src/app/components/patient-dashboard/patient-dashboard.component.ts
+++ src/app/components/patient-dashboard/patient-dashboard.component.ts
@@ -9,35 +9,76 @@ export class PatientDashboardComponent {
   previous_appointments=true
   patient_id: number = -1;
   patient_details:any;
-   }
-   constructor(private patientservice:PatientService)
-   {
+  sidenav!: MatSidenav;
+
+  constructor(private router: Router,private patientservice:PatientService, private observer: BreakpointObserver, private loginservice: LoginserviceService) { 
+    this.previous_appointments=true;
     this.getPatientDetails();
-   }
- 
-   ngOnInit(): void {
-    
-   }
- 
-   w3_close() {
-     var box = document.getElementsByClassName("w3-sidenav") as unknown as HTMLCollectionOf<HTMLElement>;
-     box[0].style.display="None"
-    
-   }
-   w3_open() {
-     var box = document.getElementsByClassName("w3-sidenav") as unknown as HTMLCollectionOf<HTMLElement>;
-     box[0].style.display="block"
-   }
-  showpreviousappointments(){
-   this.previous_appointments=!this.previous_appointments;
   }
- 
-  getPatientDetails(){
-   console.log(localStorage.getItem("patient_details"));
-   this.patient_details = JSON.parse( localStorage.getItem("patient_details")!);
-   console.log("Sfdgsfh");
-   console.log(this.patient_details);
+
+  ngOnInit(): void {
    
   }
+
+
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1), untilDestroyed(this))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+
+    this.router.events
+      .pipe(
+        untilDestroyed(this),
+        filter((e) => e instanceof NavigationEnd)
+      )
+      .subscribe(() => {
+        if (this.sidenav.mode === 'over') {
+          this.sidenav.close();
+        }
+      });
+  }
+
+
+
+
+  w3_close() {
+    var box = document.getElementsByClassName("w3-sidenav") as unknown as HTMLCollectionOf<HTMLElement>;
+    box[0].style.display="None"
+   
+  }
+  w3_open() {
+    var box = document.getElementsByClassName("w3-sidenav") as unknown as HTMLCollectionOf<HTMLElement>;
+    box[0].style.display="block"
+  }
+
+
+ showpreviousappointments(){
+  this.previous_appointments=!this.previous_appointments;
+ }
+
+ getPatientDetails(){
+  console.log(localStorage.getItem("patient_details"));
+  this.patient_details = JSON.parse( localStorage.getItem("patient_details")!);
+  console.log("Sfdgsfh");
+  console.log(this.patient_details);
+  
+ }
+
+
+  logout() {
+    this.loginservice.patient_logout();
+    this.router.navigate(['/Patient']);
+  }
+
 }
  
